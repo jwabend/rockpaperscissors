3ea8ec90623ed1ae2ac34493979c9ff8785702d1
jwabend <63935497+jwabend@users.noreply.github.com>
2024-07-15 12:41:53 -0400

    updated scripts.js
    added function to receive human and computer choice.

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-console.log("Hello World")
+/* variables */
+function getComputerChoice(){
+    let values = [1, 2, 3];
+    let number = values[Math.floor(Math.random() * values.length)];
+    console.log(number);
+    if (number==1){
+        return "rock";
+    } else if(number ==2){
+        return "paper";
+    }
+    return "scissor"
+
+}
+
+/* functions */
+function getHumanChoice() {
+    const readlineSync = require('readline-sync');
+    let human_choice = readlineSync.question("Rock, Paper, or Scissors? ");
+
+    while (["rock", "paper", "scissors"].includes(human_choice.toLowerCase()) === false) {
+        human_choice = readlineSync.question("That's not an option, What's your move? ");
+    }
+    console.log(`You chose: ${human_choice}`);
+    return human_choice.toLowerCase();
+}
+
+
+
+
+function playRound(){
+
+}
+
+function playGame(){
+    getComputerChoice()
+    getHumanChoice()
+}
+
+playGame()
